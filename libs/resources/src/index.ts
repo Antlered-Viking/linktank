3f5d9da60294b515bf8d70f1df06a612006facbb
Codemaster Mick <51538065+codemastermick@users.noreply.github.com>
2022-03-13 19:37:49 -0400

    refactor(resources): remove defunt categories module

--- libs/resources/src/index.ts
+++ libs/resources/src/index.ts
@@ -1,7 +1,5 @@
 export * from './links/links.module';
-export * from './categories/categories.module';
 export * from './certificates/certificates.module';
 
 export * from './links/links.service';
-export * from './categories/categories.service';
 export * from './certificates/certificates.service';
