a979f271992b751e907112503df4cf40b7887c0d
Codemaster Mick <51538065+codemastermick@users.noreply.github.com>
2022-03-13 10:07:01 -0400

    feat(resources): initial commit of categories resources

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -3,11 +3,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-import { LinksController, LinksService } from '@linktank/resources';
+import {
+  LinksController,
+  LinksService,
+  CategoriesController,
+  CategoriesService,
+} from '@linktank/resources';
 
 @Module({
   imports: [],
-  controllers: [AppController, LinksController],
-  providers: [AppService, LinksService],
+  controllers: [AppController, LinksController, CategoriesController],
+  providers: [AppService, LinksService, CategoriesService],
 })
 export class AppModule {}
--- libs/resources/src/index.ts
+++ libs/resources/src/index.ts
@@ -1,3 +1,7 @@
 export * from './links/links.module';
 export * from './links/links.controller';
 export * from './links/links.service';
+
+export * from './categories/categories.module';
+export * from './categories/categories.controller';
+export * from './categories/categories.service';
