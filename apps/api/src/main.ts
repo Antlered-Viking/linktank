6e104a8ad480e7c4a6aaffedafa1c5a7e615284e
Codemaster Mick <51538065+codemastermick@users.noreply.github.com>
2022-03-13 16:30:50 -0400

    chore(api): set default version to 1

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -12,6 +12,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableVersioning({
     type: VersioningType.URI,
+    defaultVersion: '1',
   });
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
