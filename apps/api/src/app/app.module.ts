4a0890650bdcec74ebe4e5296cefcf3b3eafc072
Codemaster Mick <51538065+codemastermick@users.noreply.github.com>
2022-03-13 10:03:56 -0400

    feat(resources): initial commit of links resources

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -3,9 +3,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { LinksController, LinksService } from '@linktank/resources';
+
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, LinksController],
+  providers: [AppService, LinksService],
 })
 export class AppModule {}
--- libs/resources/src/index.ts
+++ libs/resources/src/index.ts
@@ -1,1 +1,3 @@
-export * from './lib/resources.module';
+export * from './links/links.module';
+export * from './links/links.controller';
+export * from './links/links.service';
