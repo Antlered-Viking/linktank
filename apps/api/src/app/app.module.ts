f8b42c2fffb30eece346e7cb0a26849cf86cbd26
Codemaster Mick <51538065+codemastermick@users.noreply.github.com>
2022-03-13 21:24:01 -0400

    fix(api): remove dead code

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -4,14 +4,10 @@ import { HttpModule } from '@nestjs/axios';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-import {
-  LinksModule,
-  CategoriesModule,
-  CertificatesModule,
-} from '@linktank/resources';
+import { LinksModule, CertificatesModule } from '@linktank/resources';
 
 @Module({
-  imports: [HttpModule, LinksModule, CategoriesModule, CertificatesModule],
+  imports: [HttpModule, LinksModule, CertificatesModule],
   controllers: [AppController],
   providers: [AppService],
 })
