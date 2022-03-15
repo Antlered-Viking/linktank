8d40be1017e9d14adf23f6d4259043460bf0734b
NeonSpork <leiknes@gmail.com>
2022-03-15 21:43:43 +0100

    refactor: move icons into svg's and remove unecessary components

--- libs/ui/src/lib/edit-button/edit-button.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'linktank-edit-button',
-  templateUrl: './edit-button.component.html',
-  styleUrls: ['./edit-button.component.scss'],
-})
-export class EditButtonComponent {}
--- libs/ui/src/lib/ui.module.ts
+++ libs/ui/src/lib/ui.module.ts
@@ -1,18 +1,10 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LinkComponent } from './link/link.component';
-import { ReadStateComponent } from './read-state/read-state.component';
-import { EditButtonComponent } from './edit-button/edit-button.component';
-import { DeleteButtonComponent } from './delete-button/delete-button.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [
-    LinkComponent,
-    ReadStateComponent,
-    EditButtonComponent,
-    DeleteButtonComponent,
-  ],
+  declarations: [LinkComponent],
   exports: [LinkComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
