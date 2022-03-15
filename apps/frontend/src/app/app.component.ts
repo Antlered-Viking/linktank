26c453bd9363dcf692e82b61d60dec02576d09eb
NeonSpork <leiknes@gmail.com>
2022-03-15 23:30:56 +0100

    feat: header component

--- apps/frontend/src/app/app.component.ts
+++ apps/frontend/src/app/app.component.ts
@@ -8,7 +8,7 @@ import { AppService } from './app.service';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  title = 'frontend';
+  title = 'linktank';
   links: Link[];
 
   constructor(private appService: AppService) {
--- libs/ui/src/lib/ui.module.ts
+++ libs/ui/src/lib/ui.module.ts
@@ -2,11 +2,12 @@ import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { LinkComponent } from './link/link.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   imports: [CommonModule, HttpClientModule],
-  declarations: [LinkComponent],
-  exports: [LinkComponent],
+  declarations: [LinkComponent, HeaderComponent],
+  exports: [LinkComponent, HeaderComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class UiModule {}
