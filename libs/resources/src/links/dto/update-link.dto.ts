03779c25d54edccb9a12bbebfea37ce2640b5940
Codemaster Mick <51538065+codemastermick@users.noreply.github.com>
2022-03-15 18:46:36 -0400

    fix(resources): add default constructors to DTOs

--- libs/resources/src/links/dto/update-link.dto.ts
+++ libs/resources/src/links/dto/update-link.dto.ts
@@ -5,4 +5,17 @@ export class UpdateLinkDto extends PartialType(CreateLinkDto) {
   isRead?: boolean;
   notes?: string;
   customData?: string[];
+
+  constructor(
+    url = 'INVALID',
+    tags: string[] = [],
+    isRead = false,
+    notes = '',
+    customData: string[] = []
+  ) {
+    super(url, tags);
+    this.isRead = isRead;
+    this.notes = notes;
+    this.customData = customData;
+  }
 }
