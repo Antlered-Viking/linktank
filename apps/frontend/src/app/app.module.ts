9215f2aadd68f7232b1d15e7f36464ebb849d56c
Codemaster Mick <51538065+codemastermick@users.noreply.github.com>
2022-03-16 14:43:06 -0400

    chore(frontend): allow custom elements

--- apps/frontend/src/app/app.module.ts
+++ apps/frontend/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
@@ -24,5 +24,6 @@ import { UiModule } from '@linktank/ui';
   ],
   providers: [AppService],
   bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppModule {}
