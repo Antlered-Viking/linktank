a517ec37cb3d41ceb047658e6403df7d6c8f501a
NeonSpork <leiknes@gmail.com>
2022-03-17 00:05:01 +0100

    fix: routing not doubled, but angular complains about no home

--- apps/frontend/src/app/app-routing.module.ts
+++ apps/frontend/src/app/app-routing.module.ts
@@ -16,7 +16,7 @@ const routes: Routes = [
       },
     ],
   },
-  { path: 'home', redirectTo: '/home', pathMatch: 'full' },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
 
 @NgModule({
