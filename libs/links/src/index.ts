5870db6af6f46cd23de8a19c80be3c812a01a919
Codemaster Mick <51538065+codemastermick@users.noreply.github.com>
2022-03-16 15:39:46 -0400

    refactor(links): flatten structure

--- libs/links/src/index.ts
+++ libs/links/src/index.ts
@@ -1,7 +1,7 @@
-export * from './links/links.module';
-export * from './links/links.service';
-export * from './links/links.health';
+export * from './links.module';
+export * from './links.service';
+export * from './links.health';
 
-export * from './links/dto/create-link.dto';
-export * from './links/dto/update-link.dto';
-export * from './links/entities/link.entity';
+export * from './dto/create-link.dto';
+export * from './dto/update-link.dto';
+export * from './entities/link.entity';
