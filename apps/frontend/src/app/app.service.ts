d696df0eaadb77546e011f239c6d42c75bd08932
Codemaster Mick <51538065+codemastermick@users.noreply.github.com>
2022-03-17 12:27:28 -0400

    chore(frontend): deal with todo item

--- apps/frontend/src/app/app.service.ts
+++ apps/frontend/src/app/app.service.ts
@@ -1,18 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { lastValueFrom } from 'rxjs';
-// import { Link } from '@prisma/client'; //TODO replace this with an internal type
 import { environment } from '../environments/environment';
-
-interface Link {
-  id: string;
-  url: string;
-  isRead: boolean;
-  tags: string[];
-  notes: string;
-  customData: string[];
-  metadataId: string;
-}
+import { Link } from '@linktank/links';
 
 @Injectable({
   providedIn: 'root',
