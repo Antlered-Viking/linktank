29faaeb4f778732a479bf7d12af576145cc2802b
Codemaster Mick <51538065+codemastermick@users.noreply.github.com>
2022-03-17 12:38:21 -0400

    chore(health): make failing service clear it is temporary

--- libs/health/src/lib/check.controller.ts
+++ libs/health/src/lib/check.controller.ts
@@ -20,11 +20,9 @@ export class CheckController {
     return this.health.check([
       () => this.http.pingCheck('API', 'http://localhost:3333/api/v1'),
       () => this.links.isHealthy('Link Service'),
+      //TODO remove next check once the UI has been created and styled
       () =>
-        this.http.pingCheck(
-          'Certificate Service',
-          'http://localhost:3333/api/v1/certificates/status'
-        ),
+        this.http.pingCheck('Mock Service', 'http://localhost:3333/api/v1/404'),
       () => this.http.pingCheck('Frontend', 'http://localhost:4200'),
     ]);
   }
