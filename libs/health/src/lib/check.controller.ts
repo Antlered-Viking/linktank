877816d9418474f2a28265dab46d37b00ef5f09c
Codemaster Mick <51538065+codemastermick@users.noreply.github.com>
2022-03-18 11:36:58 -0400

    feat(health): add user health check to module

--- libs/health/src/lib/check.controller.ts
+++ libs/health/src/lib/check.controller.ts
@@ -5,13 +5,15 @@ import {
   HealthCheck,
 } from '@nestjs/terminus';
 import { LinksHealthIndicator } from '@linktank/links';
+import { UsersHealthIndicator } from '@linktank/users';
 
 @Controller('status')
 export class CheckController {
   constructor(
     private health: HealthCheckService,
     private http: HttpHealthIndicator,
-    private links: LinksHealthIndicator
+    private links: LinksHealthIndicator,
+    private users: UsersHealthIndicator
   ) {}
 
   @Get()
@@ -20,6 +22,7 @@ export class CheckController {
     return this.health.check([
       () => this.http.pingCheck('API', 'http://localhost:3333/api/v1'),
       () => this.links.isHealthy('Link Service'),
+      () => this.users.isHealthy('User Service'),
       //TODO remove next check once the UI has been created and styled
       () =>
         this.http.pingCheck('Mock Service', 'http://localhost:3333/api/v1/404'),
