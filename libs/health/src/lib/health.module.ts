0409db93f59183d421d5d74daeabbc0450bca2a7
Codemaster Mick <51538065+codemastermick@users.noreply.github.com>
2022-03-18 11:36:58 -0400

    feat(health): add user health check to module

--- libs/health/src/lib/health.module.ts
+++ libs/health/src/lib/health.module.ts
@@ -1,12 +1,13 @@
 import { LinksHealthIndicator, LinksModule } from '@linktank/links';
+import { UsersHealthIndicator, UsersModule } from '@linktank/users';
 import { Module } from '@nestjs/common';
 import { TerminusModule } from '@nestjs/terminus';
 import { CheckController } from './check.controller';
 
 @Module({
-  imports: [TerminusModule, LinksModule],
+  imports: [TerminusModule, LinksModule, UsersModule],
   controllers: [CheckController],
-  providers: [LinksHealthIndicator],
+  providers: [LinksHealthIndicator, UsersHealthIndicator],
   exports: [],
 })
 export class HealthModule {}
