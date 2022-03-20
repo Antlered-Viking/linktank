8860101550d37379d4f245dfed1d3a2cea2f7957
Codemaster Mick <51538065+codemastermick@users.noreply.github.com>
2022-03-20 09:01:00 -0400

    chore(auth): use passport for auth mechanism

--- libs/auth/src/lib/auth.module.ts
+++ libs/auth/src/lib/auth.module.ts
@@ -1,10 +1,14 @@
+import { UsersModule } from '@linktank/users';
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { LocalStrategy } from './local.strategy';
 
 @Module({
+  imports: [UsersModule, PassportModule],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, LocalStrategy],
   exports: [AuthService],
 })
 export class AuthModule {}
