8456a0f1f774809c32346a29b4d749d4a8463825
Codemaster Mick <51538065+codemastermick@users.noreply.github.com>
2022-03-20 09:09:40 -0400

    fix(users): add missing id prop to sanitized user

--- libs/users/src/lib/entities/user.sanitized.entity.ts
+++ libs/users/src/lib/entities/user.sanitized.entity.ts
@@ -1,8 +1,10 @@
 export class SanitizedUser {
+  id: string;
   name: string;
   avatarURL: string;
 
-  constructor(name: string, avatarURL: string) {
+  constructor(id: string, name: string, avatarURL: string) {
+    this.id = id;
     this.name = name;
     this.avatarURL = avatarURL;
   }
