1e0fdcd9eefb513753166f7d657f5fce1ffd4d96
Codemaster Mick <51538065+codemastermick@users.noreply.github.com>
2022-03-20 09:00:42 -0400

    feat(auth): fill in auth controller logic

--- libs/auth/src/lib/auth.controller.ts
+++ libs/auth/src/lib/auth.controller.ts
@@ -1,7 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { CreateUserDto, SanitizedUser } from '@linktank/users';
+import { Controller, Post, Request, UseGuards, Version } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { LocalAuthGuard } from './local.guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
+
+  @Version('1')
+  @Post('register')
+  async register(@Request() req: { body: CreateUserDto }) {
+    return await this.authService.registerUser(req.body);
+  }
+
+  @Version('1')
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  async login(
+    @Request()
+    req: {
+      name: string;
+      password: string;
+      user: SanitizedUser;
+    }
+  ): Promise<SanitizedUser> {
+    return req.user;
+  }
 }
