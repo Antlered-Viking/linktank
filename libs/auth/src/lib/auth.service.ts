ca89c6d5d8e876f60c637e8ea226cdd526c94ab8
Codemaster Mick <51538065+codemastermick@users.noreply.github.com>
2022-03-20 08:58:34 -0400

    feat(auth): fill in auth service

--- libs/auth/src/lib/auth.service.ts
+++ libs/auth/src/lib/auth.service.ts
@@ -1,4 +1,21 @@
+import { CreateUserDto, SanitizedUser, UsersService } from '@linktank/users';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private users: UsersService) {}
+
+  async registerUser(user: CreateUserDto): Promise<SanitizedUser> {
+    return await this.users.create(user);
+  }
+
+  async validateUser(name: string, pass: string): Promise<SanitizedUser> {
+    const user = await this.users.findOne(name);
+    if (user && user.name === name && user.password === pass) {
+      const { password, ...result } = user;
+      return result;
+    }
+    return undefined;
+    // return user;
+  }
+}
