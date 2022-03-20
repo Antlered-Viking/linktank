24dfa26f4872ac2398b680d8edb07892ea9d9a18
Codemaster Mick <51538065+codemastermick@users.noreply.github.com>
2022-03-20 12:02:22 -0400

    feat: enable RBAC

--- libs/users/src/lib/entities/user.sanitized.entity.ts
+++ libs/users/src/lib/entities/user.sanitized.entity.ts
@@ -2,10 +2,12 @@ export class SanitizedUser {
   id: string;
   name: string;
   avatarURL: string;
+  roles: string[];
 
-  constructor(id: string, name: string, avatarURL: string) {
+  constructor(id: string, name: string, avatarURL: string, roles: string[]) {
     this.id = id;
     this.name = name;
     this.avatarURL = avatarURL;
+    this.roles = roles;
   }
 }
