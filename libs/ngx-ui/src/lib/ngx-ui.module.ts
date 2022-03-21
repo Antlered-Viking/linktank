dcd607070499136139f6deacc16b152c64361387
Codemaster Mick <51538065+codemastermick@users.noreply.github.com>
2022-03-21 13:16:13 -0400

    feat(ngx-ui): move eye icon to component

--- libs/ngx-ui/src/lib/ngx-ui.module.ts
+++ libs/ngx-ui/src/lib/ngx-ui.module.ts
@@ -18,6 +18,7 @@ import { TagIconComponent } from './tag-icon/tag-icon.component';
 import { DeleteIconComponent } from './delete-icon/delete-icon.component';
 import { UndoIconComponent } from './undo-icon/undo-icon.component';
 import { CheckIconComponent } from './check-icon/check-icon.component';
+import { EyeIconComponent } from './eye-icon/eye-icon.component';
 
 @NgModule({
   imports: [CommonModule, FormsModule, HttpClientModule, RouterModule],
@@ -37,6 +38,7 @@ import { CheckIconComponent } from './check-icon/check-icon.component';
     DeleteIconComponent,
     UndoIconComponent,
     CheckIconComponent,
+    EyeIconComponent,
   ],
   exports: [
     LinkComponent,
@@ -52,6 +54,7 @@ import { CheckIconComponent } from './check-icon/check-icon.component';
     DeleteIconComponent,
     UndoIconComponent,
     CheckIconComponent,
+    EyeIconComponent,
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
