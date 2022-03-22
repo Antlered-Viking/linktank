9ea5a6671d0adf5077526c46a7f74cf13883b5da
Codemaster Mick <51538065+codemastermick@users.noreply.github.com>
2022-03-22 17:18:14 -0400

    chore(frontend): clean up code

--- apps/frontend/src/app/app.service.ts
+++ apps/frontend/src/app/app.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { lastValueFrom } from 'rxjs';
-import { environment } from '../environments/environment';
 import { Link } from '@linktank/links';
 
 @Injectable({
@@ -12,16 +11,14 @@ export class AppService {
 
   public async getLinks(): Promise<Link[]> {
     const res = await lastValueFrom(
-      this.http.get<{ data: Link[] }>(
-        `${environment.apiEndpoint}/links?expand=metadata,tags`
-      )
+      this.http.get<{ data: Link[] }>('api/v1/links?expand=metadata,tags')
     );
     return res.data;
   }
   public async getFilteredLinks(filter: string): Promise<Link[]> {
     const res = await lastValueFrom(
       this.http.get<{ data: Link[] }>(
-        `${environment.apiEndpoint}/links?expand=metadata,tags&filter=${filter}`
+        `api/v1/links?expand=metadata,tags&filter=${filter}`
       )
     );
     return res.data;
