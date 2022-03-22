3cbabe7e2225345dc068f12c70916642c5b47235
Codemaster Mick <51538065+codemastermick@users.noreply.github.com>
2022-03-22 19:59:20 -0400

    chore(ngx-ui): couple auth panel and login more tightly

--- libs/ngx-ui/src/lib/auth-panel/auth-panel.component.ts
+++ libs/ngx-ui/src/lib/auth-panel/auth-panel.component.ts
@@ -1,6 +1,7 @@
 import { trigger, transition, style, animate } from '@angular/animations';
 import { Component } from '@angular/core';
 import { UserService } from '@linktank/ngx-auth';
+import { SanitizedUser } from '@linktank/users';
 
 @Component({
   selector: 'linktank-auth-panel',
@@ -10,17 +11,31 @@ import { UserService } from '@linktank/ngx-auth';
     trigger('slideInOut', [
       transition(':enter', [
         style({ transform: 'translateX(150%)' }),
-        animate('1000ms ease-in', style({ transform: 'translateX(0%)' })),
+        animate('500ms ease-in', style({ transform: 'translateX(0%)' })),
       ]),
       transition(':leave', [
-        animate('1000ms ease-in', style({ transform: 'translateX(150%)' })),
+        animate('500ms ease-in', style({ transform: 'translateX(150%)' })),
       ]),
     ]),
   ],
 })
 export class AuthPanelComponent {
   visible: boolean;
+  isLoggedIn: boolean;
+  user?: SanitizedUser;
+
   constructor(private userService: UserService) {
     this.visible = this.userService.user === undefined;
+    this.isLoggedIn = this.userService.user !== undefined;
+  }
+
+  toggleVisibility() {
+    this.visible = !this.visible;
+    this.isLoggedIn = this.userService.user !== undefined;
+  }
+
+  updateUser(user: SanitizedUser | undefined) {
+    this.user = user;
+    this.toggleVisibility();
   }
 }
--- libs/ngx-ui/src/lib/login/login.component.ts
+++ libs/ngx-ui/src/lib/login/login.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { UserService } from '@linktank/ngx-auth';
+import { SanitizedUser } from '@linktank/users';
 
 @Component({
   selector: 'linktank-login',
@@ -11,15 +12,21 @@ export class LoginComponent {
   password: string;
   @Input()
   usePin: boolean;
+  isLoggedIn: boolean;
+  @Output()
+  authChangedEvent = new EventEmitter<SanitizedUser | undefined>();
+
   constructor(private user: UserService) {
     this.name = '';
     this.password = '';
     this.usePin = true;
+    this.isLoggedIn = false;
   }
 
   async submit() {
     await this.user.login(this.name, this.password);
     this.name = '';
     this.password = '';
+    this.authChangedEvent.emit(this.user.user);
   }
 }
