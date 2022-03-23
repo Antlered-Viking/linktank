dc9d8a3b56685b2f821b848babbf31505cf45367
Codemaster Mick <51538065+codemastermick@users.noreply.github.com>
2022-03-23 14:30:16 -0400

    chore(frontend): remove dead code

--- apps/frontend/src/app/app-routing.module.ts
+++ apps/frontend/src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
 import { NgxUiModule, WelcomeComponent } from '@linktank/ngx-ui';
 import {
   LinkComponent,
