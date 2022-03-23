43801729158108355340c798ed140e9e0114642b
Codemaster Mick <51538065+codemastermick@users.noreply.github.com>
2022-03-23 13:53:49 -0400

    chore(frontend): remove dead code

--- apps/frontend/src/app/app.component.ts
+++ apps/frontend/src/app/app.component.ts
@@ -1,29 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { Link } from '@linktank/links';
-import { UserService } from '@linktank/ngx-auth';
-import { AppService } from './app.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'linktank-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'linktank';
-  links: Link[];
-
-  constructor(
-    private appService: AppService,
-    private userService: UserService
-  ) {
-    this.links = [];
-  }
-  async ngOnInit(): Promise<void> {
-    if (this.userService.user) {
-      this.links = await this.appService.getLinks();
-    }
-  }
-  async updateLinks(filter: string): Promise<void> {
-    this.links = await this.appService.getFilteredLinks(filter);
-  }
 }
